c49bdeb241599ea88817b6dcd3aaaa6b459d5c09
WingsUpete <petershen815@126.com>
2022-04-06 13:28:07 +0800

    [SETUP] Adds comparison function to check the validity of the result

--- mpi_matrix.c
+++ mpi_matrix.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAT_SIZE 500
 
@@ -16,6 +17,18 @@ void brute_force_matmul(double mat1[MAT_SIZE][MAT_SIZE], double mat2[MAT_SIZE][M
     }
 }
 
+int checkRes(double target[MAT_SIZE][MAT_SIZE], double res[MAT_SIZE][MAT_SIZE]) {
+   /* check whether the obtained result is the same as the intended target; if true return 1, else return 0 */
+   for (int i = 0; i < MAT_SIZE; ++i) {
+      for (int j = 0; j < MAT_SIZE; ++j) {
+         if (res[i][j] != target[i][j]) {
+            return 0;
+         }
+      }
+   }
+   return 1;
+}
+
 int main(int argc, char *argv[])
 {
    int rank;
@@ -26,6 +39,10 @@ int main(int argc, char *argv[])
        bfRes[MAT_SIZE][MAT_SIZE];   /* brute force result bfRes */
 
    /* You need to intialize MPI here */
+   MPI_Init(NULL, NULL);
+
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
 
    if (rank == 0)
    {
@@ -53,7 +70,11 @@ int main(int argc, char *argv[])
       /* Compare results with those from brute force */
       brute_force_matmul(a, b, bfRes);
       
-
+      int same = checkRes(bfRes, c);
+      if (!same) {
+         fprintf(stderr, "ERROR: Your calculation is not the same as the brute force result, please check!\n");
+         exit(-1);
+      }
    }
    else
    {
@@ -62,4 +83,5 @@ int main(int argc, char *argv[])
    }
 
    /* Don't forget to finalize your MPI application */
+   MPI_Finalize();
 }
